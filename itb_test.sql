5a8ea2705e12ed6896bd26db916c3045f6bbac71
dr-matt-smith <dr_matt_smith@me.com>
2016-05-07 15:23:23 +0100

    another attempt from live dump

--- itb_test.sql
+++ itb_test.sql
@@ -3,28 +3,44 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: May 07, 2016 at 01:14 PM
+-- Generation Time: May 07, 2016 at 04:22 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+--
+-- Database: `itb_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `id` int(11) NOT NULL,
   `description` text NOT NULL,
   `price` float NOT NULL,
   `quantityInStock` int(11) NOT NULL,
-  `restockQuantity` int(11) NOT NULL,
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=5;
+  `restockQuantity` int(11) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 
--- CREATE TABLE `products` (
---   `id` int(11) NOT NULL PRIMARY KEY AUTOINCREMENT,
---   `description` text NOT NULL,
---   `price` float NOT NULL,
---   `quantityInStock` int(11) NOT NULL,
---   `restockQuantity` int(11) NOT NULL,
---   PRIMARY KEY (id)
--- ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=5;
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
